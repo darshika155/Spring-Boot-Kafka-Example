8549f6de7e46efa307e022457dda5268f1097acf
darshika155 <darshika155@gmail.com>
2021-01-07 16:58:09 +0530

    Initial Commit
    Uploading all the files

---
+++ springbootKafka/src/main/java/constant/Userresource.java
@@ -0,0 +1,26 @@
+package constant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class Userresource {
+	
+	@Autowired
+	KafkaTemplate<String,String> kafkatemplate;
+	private static final String TOPIC = "Kafka_Example";
+	
+	@GetMapping("/publish/message")
+	public String post(@PathVariable("message") final String message) {
+		
+		kafkatemplate.send(TOPIC, message);
+		return "Published Successfully";
+		
+	}
+
+}
